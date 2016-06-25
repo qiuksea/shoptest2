53258c105c5464680855ae71147d6432aad7ec91
Qi Jingyu <qijingyu@hotmail.com>
2016-06-26 00:31:46 +0100

    add active to products tbl; add seed for products tbl

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624193951) do
+ActiveRecord::Schema.define(version: 20160625232012) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160624193951) do
     t.integer  "updater_id",  limit: 4
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.boolean  "active"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+Product.delete_all
+Product.create! id: 1, title: "Red Seedless Grapes", description: "Creature herb light moveth gathered said unto greater third for given whales he. ",
+                        basic_price: 1.50, sold_price: 1.30, active: true
+Product.create! id: 2, title: "Strawberries 400G", description: "His Created. Light grass. Dry. Behold beast fill all, day. Third which over dominion, their.",
+                        basic_price: 2.00, sold_price: 1.78, active: true
+Product.create! id: 3, title: "Rosedene Farms Blueberries 125G", description: "They're bearing seed be days have void years living lesser they're in evening.",
+                        basic_price: 1.50, sold_price: 1.50, active: true
+Product.create! id: 4, title: "Clementine Or Sweet Easy Peeler Pack 600G", description: "Void living green creeping have second in whales abundantly own you're.",
+                        basic_price: 1.99, sold_price: 1.99, active: true
+Product.create! id: 5, title: "Organic Fair Trade Bananas 6 Pack",description: "Them third thing is image all day stars subdue. The fowl. Moving tree were you air give seasons without.",
+                        basic_price: 1.79, sold_price: 1.39, active: true
+Product.create! id: 6, title: "Green Seedless Grapes Pack 500G", description: "Moving sea, herb. Green saying, us second good female kind seas fourth.",
+                        basic_price: 1.99, sold_price:1.50 , active: true
