68e09d8fb684a3d98e5be1394d7bb5b187e6f37b
Qi Jingyu <qijingyu@hotmail.com>
2016-06-27 22:45:28 +0100

    scaffold carts controller, scaffold line_items controller, update their model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
   #get 'store/index'
   #root to: 'visitors#index'
   root to:'store#index'
+  resources :carts, only: [:show]
+  resources :line_items, only: [:create, :update, :destroy]
+
   devise_for :users
   resources :users
+
   namespace :admin do
     resources :products
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626214338) do
+ActiveRecord::Schema.define(version: 20160627124403) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id", limit: 4
+    t.integer  "cart_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
 
   create_table "product_pictures", force: :cascade do |t|
     t.integer  "product_id",         limit: 4
@@ -56,4 +70,5 @@ ActiveRecord::Schema.define(version: 20160626214338) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "line_items", "carts"
 end
