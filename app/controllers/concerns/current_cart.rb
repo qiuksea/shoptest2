da603cb0f86ee94cbe61c20590fbb1ea63720750
Qi Jingyu <qijingyu@hotmail.com>
2016-06-27 13:52:03 +0100

    scaffold Cart, scaffold LineItem

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,13 @@
+module CurrentCart
+  extend ActiveSupport::Concern
+  private
+
+  def set_cart
+    if session[:cart_id].present?
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,19 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :product_pictures,
                                 allow_destroy: true,
                                 reject_if: proc { |attributes| attributes['image'].blank? } # it points to picture model attrs
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+   # ensure that there are no line items referencing this product
+   def ensure_not_referenced_by_any_line_item
+     if line_items.empty?
+         return true
+         else
+             errors.add(:base, 'Line Items present')
+             return false
+             end
+   end
+
 end
