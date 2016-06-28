3c82ae40708478a24d21d03762369d270b465580
Qi Jingyu <qijingyu@hotmail.com>
2016-06-28 01:17:48 +0100

    add item-quantity for cart, add store/index was/now price, add methods for line-item model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show]
-  #sinclude CurrentCart
+  #include CurrentCart
   # GET /carts
   # GET /carts.json
   # def index
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,5 +2,36 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
 
-  
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :product_present
+  validate :order_present
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      product.sold_price
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+  private
+  def product_present
+    if product.nil?
+      errors.add(:product, "is not valid or is not active.")
+    end
+  end
+
+  def cart_present
+    if cart.nil?
+      errors.add(:cart, "is not a valid order.")
+    end
+  end
+
+
+
+
 end
