4883ce28f2642c3ca0a6e5091d94a099bb07432d
Qi Jingyu <qi.jingyu@agcas.org.uk>
2016-07-06 14:03:17 +0100

    add admin to users tbl and set require_admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,14 @@
 class Admin::ApplicationController < ApplicationController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
+  before_action :require_admin
+
+
+
+  def require_admin
+    unless current_user.try(:admin?)
+      flash[:notice] = "You are not allowed to access the part."
+      redirect_to root_path
+    end
+  end
 
 end
